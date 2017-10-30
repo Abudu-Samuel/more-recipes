68faabaff3034ca83c385e11d1b63259085a6a04
Balogun Abiodun <delighteddell@gmail.com>
2017-10-30 12:59:37 +0100

    associate user-model with recipe model

--- server/server/models/user.js
+++ server/server/models/user.js
@@ -3,16 +3,25 @@ module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
     username: {
       type: DataTypes.STRING,
-
+      allowNull: false
     },
     password: {
       type: DataTypes.STRING,
-    
+      allowNull: false
     },
     email: {
       type: DataTypes.STRING,
-    
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
     }
   });
+  user.associate = (models) => {
+    user.hasMany(models.recipe, {
+      foreignKey: 'userId',
+      as: 'recipes'
+    });
+  };
   return user;
 };
