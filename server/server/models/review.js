128ca953f58e0a70fa6a7988d1f5667149fe55da
Balogun Abiodun <delighteddell@gmail.com>
2017-10-30 15:05:13 +0100

    add association to models

--- server/server/models/review.js
+++ server/server/models/review.js
@@ -5,5 +5,15 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.TEXT
     }
   });
+  review.associate = (models) => {
+    review.belongsTo(models.recipe, {
+      foreignKey: 'recipeId',
+      onDelete: 'CASCADE'
+    });
+    review.belongsTo(models.user, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
+  };
   return review;
 };
