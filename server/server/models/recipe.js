a668eebccaa8c85cbed3eaeaa1657acb02bfad86
Balogun Abiodun <delighteddell@gmail.com>
2017-11-02 04:12:01 +0100

    change view model

--- server/server/models/recipe.js
+++ server/server/models/recipe.js
@@ -67,7 +67,7 @@ module.exports = (sequelize, DataTypes) => {
     });
     recipe.hasMany(models.view, {
       foreignKey: 'recipeId',
-      as: 'views'
+      as: 'view'
     });
     recipe.hasMany(models.favorite, {
       foreignKey: 'recipeId',
