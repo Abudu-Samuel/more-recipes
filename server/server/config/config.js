8cb87262b6cff2c9f3d7d65231cc69b0dd375c92
Balogun Abiodun <delighteddell@gmail.com>
2017-11-03 02:07:12 +0100

    fix bug for heroku

--- server/server/config/config.js
+++ server/server/config/config.js
@@ -8,7 +8,6 @@ module.exports = {
     username: process.env.DATABASE_DEV,
     password: process.env.DATABASE_DEVKEY,
     database: process.env.DATABASE_DB,
-    operatorsAliases: Sequelize.Op,
     host: '127.0.0.1',
     dialect: 'postgres'
   },
@@ -17,12 +16,10 @@ module.exports = {
     password: null,
     database: 'database_test',
     host: '127.0.0.1',
-    operatorsAliases: Sequelize.Op,
     dialect: 'postgres'
   },
   production: {
     DATABASE_URL: 'USE_ENV_VARIABLE',
-    operatorsAliases: Sequelize.Op,
     dialect: 'postgres'
   }
 };
